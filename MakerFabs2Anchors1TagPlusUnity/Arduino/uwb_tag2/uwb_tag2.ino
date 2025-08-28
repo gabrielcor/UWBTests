687be47ca765d0859a74a2e565d5875b3da12461
Gabriel Cor <gabrielcor@hotmail.com>
2025-08-28 19:34:41 -0400

    Intentar filtering que no funcionó

--- MakerFabs2Anchors1TagPlusUnity/Arduino/uwb_tag2/uwb_tag2.ino
+++ MakerFabs2Anchors1TagPlusUnity/Arduino/uwb_tag2/uwb_tag2.ino
@@ -44,8 +44,9 @@ void setup()
     DW1000Ranging.attachNewRange(newRange);
     DW1000Ranging.attachNewDevice(newDevice);
     DW1000Ranging.attachInactiveDevice(inactiveDevice);
-    //Enable the filter to smooth the distance
-    //DW1000Ranging.useRangeFilter(true);
+    // Enable the filter to smooth the distance - Does NOT work
+    // TODO: implemente a better filtering strategy
+    // DW1000Ranging.useRangeFilter(true);
 
     //we start the module as a tag
     DW1000Ranging.startAsTag("7E:00:22:EA:82:60:3B:9C", DW1000.MODE_LONGDATA_RANGE_LOWPOWER);
